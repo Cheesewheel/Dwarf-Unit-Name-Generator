27b7bc19ce3a2fc5381b4d59cb0e73eeaf0b786a
Cheesewheel <cheesewheel1573@gmail.com>
2022-01-31 00:16:05 +0100

    basic functionality done

--- Program.cs
+++ Program.cs
@@ -1,32 +1,137 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("'ate orks");
-Console.WriteLine("'ate elves");
+﻿// Initialize things to choose from
+string[] units = initializeGeneralUnits();
+string[] melee = initializeMelee();
+string[] blunt = initializeBlunt();
+string[] cut = initializeCut();
+string[] pierce = initializePierce();
+string[] ranged = initializeRanged();
+string[] adjectives = initializeAdjectives();
 
-string[] armor = initializeArmor();
-string[] units = initializeUnits();
-string[] weapons = initializeWeapons();
+// Console.WriteLine("Specify or leave empty for total random.");
+// System.Console.WriteLine("-w [weapontype] for weapon type");
 
-static string[] initializeArmor(){
-    string[] list = {};
+Random rnd = new Random();
+units = units.Concat(melee).ToArray();
+units = units.Concat(blunt).ToArray();
+units = units.Concat(cut).ToArray();
+units = units.Concat(pierce).ToArray();
+units = units.Concat(ranged).ToArray();
+int iUnit = rnd.Next(0, units.Length);
+int iAdjective = rnd.Next(0, adjectives.Length);
 
-    return list;
-}
+System.Console.WriteLine("6. {0} {1}", adjectives[iAdjective], units[iUnit]);
 
-static string[] initializeUnits(){   
+
+static string[] initializeGeneralUnits(){   
     string[] list = {
+        "[weapon the squad uses]",
         "Battalion",
         "Brigade",
         "Company",
         "Division",
+        "Fighters",
+        "Fists",
         "Guard",
-        "Hunters",
+        "Hands",
+        "Helmets",
+        "Killers",
         "Legion",
-        "Rangers",
+        "Raiders",
         "Regiment",
-        "Shooters",
-        "Skirmishers",
+        "Scars",
+        "Soldiers",
+        "Sons",
         "Squadron",
+        "Volunteers",
+        "Warriors",
         "Watch"
+    };
+
+    return list;
+}
+
+static string[] initializeMelee(){   
+    string[] list = {
+        "Bearers",
+        "Brawlers",
+        "Holders",
+        "Linebreakers",
+        "Lineholders",
+        "Shields",
+        "Stormers"
+    };
+
+    return list;
+}
+
+static string[] initializeBlunt(){   
+    string[] list = {
+        "Bashers",
+        "Breakers",
+        "Bruisers",
+        "Crushers",
+        "Smashers"
+    };
+
+    return list;
+}
+
+static string[] initializeCut(){   
+    string[] list = {
+        "Cutters",
+        "Choppers",
+        "Slashers"
+    };
+
+    return list;
+}
+
+static string[] initializePierce(){   
+    string[] list = {
+        "Porcupines"
+    };
+
+    return list;
+}
+
+static string[] initializeRanged(){   
+    string[] list = {
+        "Arbalists",
+        "Arrows",
+        "Bolts",
+        "Hunters",
+        "Marksdwarves",
+        "Quarrellers",
+        "Rangers",
+        "Shooters",
+        "Skirmishers"
+    };
+
+    return list;
+}
+
+static string[] initializeAdjectives(){   
+    string[] list = {
+        "[Current leader of the fort's]",
+        "[First expedition leader's]",
+        "[Fort name's]",
+        "[Squad leader's]",
+        "Black",
+        "Brave",
+        "Brown",
+        "Blue",
+        "Golden",
+        "Green",
+        "Grey",
+        "Iron",
+        "Metal",
+        "Orange",
+        "Red",
+        "Shining",
+        "Silver",
+        "Steel",
+        "White",
+        "Yellow"
     };
 
     return list;
